5fa6a20d2b5b000d7c44d276f4a51763a4eaf791
Sefa Bilicier <109534799+sefabilicier@users.noreply.github.com>
2026-06-12 09:29:14 +0300

    Update test_users.py

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,5 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from fastapi.testclient import TestClient
-from main import app
+from app.main import app  # app.main, sadece main değil
 
 client = TestClient(app)
 
